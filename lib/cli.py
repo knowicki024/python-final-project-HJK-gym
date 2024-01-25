c4b8c93040a64f93a370038cb1ae66c42a39be7b
Jeffrey Davis <j.davis081248@gmail.com>
2024-01-25 15:09:21 -0600

    Still working on CRUD in CLI for location, trainer, programs

--- lib/cli.py
+++ lib/cli.py
@@ -23,23 +23,43 @@ def main():
         if choice == "0":
             exit_program()
         elif choice == "1":
-            add_member()
-        elif choice == "2":
-            change_membership()
-        elif choice == "3":
             view_members()
+        elif choice == "2":
+            add_member()
+        elif choice == "3":
+            change_membership()
         elif choice == "4":
-            view_all_programs()
-        elif choice == "5":
-            add_program()
-        elif choice == "6":
             delete_member()
-        elif choice == "7":
-            delete_program()
-        elif choice == "8":
+        elif choise == "5":
+            view_all_trainers() 
+        elif choice == "6":
             add_trainer()
-        elif choice == "9":
+        elif choice == "7":
+            update_trainer():
+            pass
+        elif choice == "8":
             delete_trainer()
+        elif choice == "9":
+            view_all_locations()
+        elif choice == "10":
+            add_location()
+            pass
+        elif choice == "11":
+            update_location()
+            pass
+        elif choice == "12":
+            delete_location()
+            pass
+        elif choice == "13":
+            view_all_programs()
+        elif choice == "14":
+            add_program()
+        elif choice == "15":
+            update_program()
+            pass
+        elif choice == "16":
+            delete_program()
+
         else:
             print("Invalid choice")
 
@@ -69,15 +89,23 @@ def menu():
     console.print("Welcome to JKH Gym", style="bold blue")
     console.print("Please select an option:", style="bold")
     console.print("0. Exit the program")
-    console.print("1. Add New Gym Member")
-    console.print("2. Change Membership Plan")
-    console.print("3. View Members")
-    console.print("4. View All Programs")
-    console.print("5. Add New Exercise Program")
-    console.print("6. Delete Members")
-    console.print("7. Delete Exercise Program")
-    console.print("8. Add Trainer")
-    console.print("9. Delete Trainer")
+    console.print("1. View Members")
+    console.print("2. Add New Member")
+    console.print("3. Change Membership Plan")
+    console.print("4. Delete Member")
+    console.print("5. View Trainers")
+    console.print("6. Add Trainer")
+    console.print("7. Update Trainer")
+    console.print("8. Delete Trainer")
+    console.print("9. View Locations")
+    console.print("10. Add Location")
+    console.print("11. Update Location")
+    console.print("12. Delete Location")
+    console.print("13. View All Programs")
+    console.print("14. Add New Exercise Program")
+    console.print("15. Update Existing Exercise Program")
+    console.print("16. Delete Exercise Program")
+
 print("Thank you for using JKH Gym.")
 
 if __name__ == "__main__":
